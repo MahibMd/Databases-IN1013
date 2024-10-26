b57ad65aec89bb204119c84f2f79d88ca5467ccd
MahibMd <Mahib.Md@city.ac.uk>
2024-10-26 13:44:27 +0100

    Delete HW 4/task 2.sql

--- HW 4/task 2.sql
+++
@@ -1,5 +0,0 @@
-UPDATE petEvent SET remark = "5 kittens, 3 female, 2 male" WHERE petname = "Fluffy";
-UPDATE petEvent SET petname = "Claws" WHERE id = 5;
-UPDATE petPet SET death = "2020-09-01" WHERE petname = "Puffball";
-DELETE FROM petEvent WHERE petname = "Fluffy";
-DELETE FROM petPet WHERE petowner = "Harold";
